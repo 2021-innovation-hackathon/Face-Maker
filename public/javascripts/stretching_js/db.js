6e3e97f2dc6d46f3a0ca320560de6aa16ae6d315
namjiseong <skawltjd2000@naver.com>
2021-08-26 06:41:09 +0900

    잡동사니 정리

--- public/javascripts/stretching_js/db.js
+++
@@ -1,8 +0,0 @@
-var db_config = {
-    host:'us-cdbr-east-04.cleardb.com',
-    user:'b629f7bf92c0a3',
-    password:'cf6f0b58',
-    database:'heroku_0d9db5affa3ffb5'
-  };
-  
-module.exports = db_config;
